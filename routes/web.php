ec92a92feb2744f32af072b5d5bbdd8f81983bce
edmundogarol <s3484069@student.rmit.edu.au>
2017-09-02 23:35:43 +1000

    Fixed routes/web.php bug

--- routes/web.php
+++ routes/web.php
@@ -33,14 +33,15 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/update', function () {
     return view('tempupdateprofile');
+});
 
 Route::get('/tutor', function () {
     return view('tutor');
-
 });
+
 Route::get('/edit', function () {
     return view('edit');
-
+});
 
 Route::get('/home', 'HomeController@index');
 
